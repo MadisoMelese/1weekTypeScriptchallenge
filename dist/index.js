d0375f932ae36811257e955e43a3df4e5131599a
Madiso Melese <madishamadiso00@gmail.com>
2025-01-31 16:00:52 +0300

    Error in the tsconfig.json fixed

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,11 @@
 "use strict";
-let age = 20;
+let age = 120;
 if (age < 50) {
     console.log("You are young");
 }
+else {
+    console.log("You are old");
+    let favoriteColor = "blue";
+    console.log(favoriteColor);
+} 
 //# sourceMappingURL=index.js.map
---
+++ src/index.js
@@ -0,0 +1,4 @@
+var age = 20;
+if (age < 50) {
+    console.log("You are young");
+}
