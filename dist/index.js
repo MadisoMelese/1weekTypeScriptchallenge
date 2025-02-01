ca0f175f8fd1220c3cf56b0539b9d727a11e1e52
Madiso Melese <madishamadiso00@gmail.com>
2025-02-01 16:46:10 +0300

    updated

--- dist/index.js
+++ dist/index.js
@@ -1,11 +1,9 @@
 "use strict";
-let age = 120;
+let age = 30;
 if (age < 50) {
     console.log("You are young");
 }
 else {
     console.log("You are old");
-    let favoriteColor = "blue";
-    console.log(favoriteColor);
-} 
+}
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
-let age: number =20;
+let age: number =30;
 if(age<50){
     console.log("You are young");
+}
+else{
+    console.log("You are old");
 }
